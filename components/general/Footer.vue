039c6f7d4ac3ca17a47bc83b6194e3cfe87a3107
Hugo Negreyros <34077871+HugoAlonso79@users.noreply.github.com>
2024-02-19 21:33:38 -0500

    footer test implementation

--- components/general/Footer.vue
+++ components/general/Footer.vue
@@ -1,9 +1,38 @@
 <template>
-    <div>
-        Footer
+    <div class="footer flex justify-around p-10 bg-footer text-primary-content">
+        <nav>
+            <h6 class="footer-title">Contatti</h6> 
+            <a class="link link-hover">Branding</a>
+            <a class="link link-hover">Design</a>
+        </nav> 
+        <nav>
+            <h6 class="footer-title">Indirizzi Utili</h6> 
+            <a class="link link-hover">About us</a>
+            <a class="link link-hover">Contact</a>
+        </nav> 
+        <nav>
+            <h6 class="footer-title">Certificato ISO 9001:2015</h6> 
+            <a class="link link-hover">Terms of use</a>
+        </nav>
+        <nav>
+            <h6 class="footer-title">Ultimi articoli</h6> 
+            <a class="link link-hover">Terms of use</a>
+
+        </nav>
+    </div>
+    <div class="footer footer-center p-8 bg-footer text-primary-content">
+        <aside>
+            <p class="italic">ProgettoAssistenza Srl - P.Iva 02092140512 - Cookies Policy - Privacy Policy - Area Personale</p>
+        </aside>
     </div>
 </template>
 
 <script>
 
 </script>
+
+<style>
+.bg-footer {
+    background-color: #2161a1;
+}
+</style>
