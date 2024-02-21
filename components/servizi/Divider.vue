01d172ed572fd7ff1b366fe40f90d281bbd9a2e5
BJzero10 <jaldair2521@gmail.com>
2024-02-21 11:22:12 -0500

    divider update

--- components/servizi/Divider.vue
+++ components/servizi/Divider.vue
@@ -1,7 +1,7 @@
 <template>
           <div class="2xl:container 2xl:mx-auto lg:py-2 lg:px-20 md:py-2 md:px-6 py-2 px-4">
               <div class="flex flex-col lg:flex-row justify-between gap-8 pt-8">
-                  <div class="w-1/2 flex flex-col justify-center">
+                  <div class="md:w-1/2 flex flex-col justify-center">
                     <h1 role="heading" tabindex="0" class="text-4xl font-bold 2xl:leading-10 leading-0 text-justify text-[#144e99]">
                         Servizio 1
                     </h1>
@@ -9,15 +9,15 @@
                         Personale sanitario altamente qualificato.
                       </p>
                   </div>
-                  <div class="w-1/2">
+                  <div class="md:w-1/2">
                       <img class="w-full h-full " src="/slider_img_pa_img3.jpg" alt="A group of People" />
                   </div>
               </div>
               <div class="flex flex-col lg:flex-row justify-between gap-8 pt-16">
-                  <div class="w-1/2">
+                  <div class="md:w-1/2">
                       <img class="w-full h-full " src="/slider_img_pa_img3.jpg" alt="A group of People" />
                   </div>
-                  <div class="w-1/2 flex flex-col justify-center">
+                  <div class="md:w-1/2 flex flex-col justify-center">
                         <h1 role="heading" tabindex="0" class="text-4xl font-bold 2xl:leading-10 leading-0 text-justify text-[#144e99]">
                             Servizio 2
                         </h1>
@@ -27,7 +27,7 @@
                   </div>
               </div>
               <div class="flex flex-col lg:flex-row justify-between gap-8 pt-16">
-                  <div class="w-1/2 flex flex-col justify-center">
+                  <div class="md:w-1/2 flex flex-col justify-center">
                     <h1 role="heading" tabindex="0" class="text-4xl font-bold 2xl:leading-10 leading-0 text-justify text-[#144e99]">
                         Servizio 3
                     </h1>
@@ -35,7 +35,7 @@
                         L’intervento è organizzato entro poche ore dalla richiesta, tempo necessario per raggiungere il luogo dove erogare il servizio di assistenza. I servizi sono erogati per 24 ore su 24 e 7 giorni su 7.
                       </p>
                   </div>
-                  <div class="w-1/2">
+                  <div class="md:w-1/2">
                       <img class="w-full h-full " src="/slider_img_pa_img3.jpg" alt="A group of People" />
                   </div>
               </div>
